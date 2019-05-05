b79ac85c02dba74fbfa24c015243060128edc1f2
george <georgios.perivolarakis@gamelounge.com>
2019-05-05 19:18:51 +0200

    .

--- src/components/styled-component/index.js
+++ src/components/styled-component/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 class StyledComponent extends React.Component {
     render() {
         return (
-            
+            <div>
+                styled component
+            </div>
         )
     }
 }
